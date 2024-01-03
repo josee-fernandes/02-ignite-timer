335a917005505530c24f6ed3608b3f5444feb3d9
José Vitor dos Santos Fernandes <jose-vitor-7@hotmail.com>
2024-01-03 12:49:53 -0300

    Application structure - Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { Button } from "./components/Button";
+
 export function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <Button />
+    </>
+  );
 }
