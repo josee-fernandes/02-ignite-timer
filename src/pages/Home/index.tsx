bc7e526dd66ef86e13706322183868a6de9a3658
José Vitor dos Santos Fernandes <jose-vitor-7@hotmail.com>
2024-01-04 10:24:01 -0300

    Pages and routes - Improving inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,12 +19,20 @@ export function Home() {
             id="task"
             type="text"
             placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
           />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Banana" />
+          </datalist>
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             id="minutesAmount"
             type="number"
             placeholder="00"
+            step={5}
           />
           <span>minutos.</span>
         </FormContainer>
